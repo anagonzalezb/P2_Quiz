009fa01e4fa93287a76067bfccf2183373b6b221
anagonzalezb <36459901+anagonzalezb@users.noreply.github.com>
2018-03-08 13:47:23 +0100

    Add files via upload

---
+++ model.js
@@ -0,0 +1,97 @@
+const fs = require("fs");
+
+const DB_FILENAME = "quizzes.json";
+
+
+let quizzes = [
+  {
+  question: "¿Tu novi@ te pone los cuernos?",
+  answer: "si"
+  },
+  {
+  question: "¿Crees que conseguirás algún día a tu amor platónico?",
+  answer: "no"
+  },
+  {
+  question: "¿Morirás sol@?",
+  answer: "si"
+  },
+  {
+  question: "¿Ser teleco ayuda a ligar?",
+  answer: "no"
+  },
+  {
+  question: "¿Crees que tu pareja te querrá/quiere solo por interés?",
+  answer: "si"
+  }
+  ];
+
+const load = () => {
+  fs.readFile(DB_FILENAME, (err, data) => {
+    if (err){
+      if (err.code === "ENOENT") {
+        save();
+        return;
+      }
+      throw err;
+    }
+    let json = JSON.parse(data);
+    if (json) {
+      quizzes = json;
+    }
+  });
+};
+
+const save = () => {
+  fs.writeFile(DB_FILENAME,
+  JSON.stringify(quizzes),
+  err => {
+    if(err) throw err;
+  });
+};
+
+exports.count=()=> quizzes.length;
+ // Object.keys(quizzes).length;
+
+exports.add =(question, answer) => {
+  
+  quizzes.push({
+    question: (question || "").trim(),
+    answer: (answer || "").trim()
+  });
+  save();
+};
+
+exports.update =(id, question, answer) => {
+  
+  const quiz = quizzes[id];
+  if (typeof quiz === "undefined") {
+    throw new Error (`El valor del parámetro id no es válido`);
+  }
+  quizzes.splice(id, 1, {
+    question: (question || "").trim(),
+    answer: (answer || "").trim() 
+  });
+  save();
+};
+
+exports.getAll = () => JSON.parse(JSON.stringify(quizzes));
+
+exports.getByIndex = id => {
+  const quiz = quizzes[id];
+  if (typeof quiz === "undefined") {
+    throw new Error (`El valor del parámetro id no es válido.`);
+  }
+  return JSON.parse(JSON.stringify(quiz));
+};
+
+exports.deleteByIndex = id => {
+  const quiz = quizzes[id];
+  if (typeof quiz === "undefined") {
+    throw new Error (`El valor del parámetro id no es válido`);
+  }
+  quizzes.splice(id,1);
+  save();
+};
+
+load();
